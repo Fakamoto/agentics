3cd7e3dfaea51e5fad80d1a5069899abf25c6105
fakamoto <facundogoiriz@gmail.com>
2024-12-24 08:56:22 -0300

    .

--- src/__init__.py
+++ src/__init__.py
@@ -3,23 +3,19 @@ from .utils import (
     system_message,
     user_message,
     assistant_message,
-    tool_calls_message,
     tool_message,
-    create_tool_schema,
-    execute_tool,
-    format_tool_output,
+    tool_calls_message,
 )
 
+__version__ = "0.1.0"
 __all__ = [
-    # Main API
+    # Main classes
     "LLM",
-    # Utils
+    
+    # Utility functions
     "system_message",
     "user_message",
     "assistant_message",
-    "tool_calls_message",
     "tool_message",
-    "create_tool_schema",
-    "execute_tool",
-    "format_tool_output",
-]
+    "tool_calls_message",
+] 
